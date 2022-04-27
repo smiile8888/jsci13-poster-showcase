870ddfa1b23d1566c3bd3b7ad7fc3a369af6e86e
Pittawat2542 <Pittawat2542@users.noreply.github.com>
2022-04-27 13:58:35 +0000

    deploy: d7d74dfa869b7b989a6a4463e59e96787df200a8

---
+++ _next/static/9i2oMpshP5Ak-9Zp2VFZc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/117-b7a6776592d7498d.js","static/chunks/pages/index-406a896eeaeb6bd0.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
