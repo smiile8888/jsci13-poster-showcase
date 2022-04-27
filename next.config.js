c62affdb697e5f258fb87a7850faf96e99e9a76b
Pittawat <pittawat2542@gmail.com>
2022-04-27 20:40:06 +0700

    chore: prepare for github action

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['placekitten.com'],
   },
+  assetPrefix: isProd ? '/jsci12-poster-showcase/' : ''
 }
 
 module.exports = nextConfig
